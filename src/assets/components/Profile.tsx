998039d9aaeb955c5c4c16cc3e80ca3e1c7f86cc
gllanos09 <angel.llanos@tecsup.edu.pe>
2026-05-13 14:38:02 -0500

    cleaned app / profile

---
+++ src/assets/components/Profile.tsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+    return (
+        <header>
+            <h1>Gabriel Llanos</h1>
+            <h2>Frontend Developer</h2>
+        </header>
+    );
+};
+
+export default Profile;
